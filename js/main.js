caf164e5a411641d28e2f93c757616df8921ec32
DecimTheJudge <paolo1piga@gmail.com>
2024-06-20 00:28:00 +0200

    completato

--- js/main.js
+++ js/main.js
@@ -1,32 +1,78 @@
-// creo un Array
-const numeri = [];
+// creo il randomizer
+let random = numeriRandom (1, 99, 5);
+const messaggio = document.getElementById(`randomNum`);
+messaggio.innerHTML = random;
+console.log(random);
 
+// FUNZIONE DA RIGA 9 A RIGA 35 COPIATA DALLA CORREZIONE DELL'ESERCIZIO
 
-
-// creo un mathfloor random per creare il randomizer di numeri, ho scelto di fare da 0 a 10 e poi scriverlo sull'h1
-//  lo metto all'interno di un ciclo per ripetere sino a 5 numeri
-while (numeri.length <5 ){
-    numeri  = Math.floor(Math.random() * 11);
-     
+// CREA ELEMENTO CON CLASSE
+function created (tag, cssClass) {
+    const element = document.createElement (tag);
+    element.classList.add(cssClass);
+    return element
 }
 
-// voglio far vedere i numeri sull'html
-let finali = document.getElementById(`randomNum`);
-finali.append(numeri);
-console.log(numeri);
+// CREA NUMERO CASUALE IN UN RANGE
+function randomNumberRange (min, max){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// GENERA NUMERI CASUALI IN UN RANGE DI NUMERI
+function numeriRandom (min, max, count){
+    // Crea array vuoto
+    const numberList = [];
+
+    // Crea un ciclo che genera 16 numeri casuali
+    while (numberList.length < count){
+        let randomNumber = randomNumberRange (min, max);
+
+        // inserisci il numero casuale solo se non è gia presente nell'array
+        if(!numberList.includes(randomNumber)){
+            numberList.push(randomNumber);
+        }
+    }
+    return numberList
+}
+
+
+
+// variabile che gestisce i secondi
+let timerContatore = 0;
+// contatore 
+const contatore = document.getElementById(`counterSecondi`);
+
+
 
 // creo il set timeout per i 30 secondi e creo una funzione per indicare il tempo scaduto
-setTimeout(timer, 30000);
-function timer(){
-    // faccio sparire i numeri
-    finali.remove(numeri);
-    alert(`Inizia il gioco!`);
-    // creo un ciclo dove l'utente inserisce i numeri uno ad uno con un prompt
-    for( i = 0; i < 5; i++){
-        let userChoice = prompt(`Inserisci i numeri che hai visto`);
-        console.log(userChoice);
+let clock = setInterval(function(){
+    timerContatore++;
+    contatore.innerHTML = timerContatore;
+}, 10000)
+
+// faccio scomparire i numeri finito il tempo
+setTimeout(function(){
+messaggio.classList.add("sparire");
+contatore.classList.add("sparire");
+clearInterval(clock);
+}, 1000 * 30)
+
+// creo un array vuoto
+const listaNumeri = [];
+setTimeout(function(){
+    // creo un ciclo dove l'utente deve inserire i numeri
+    for (let i = 0; i < random.length; i++){
+        userInput = parseInt(prompt("Inserisci i numeri che hai visto uno alla volta"));
+        
+        if (random.includes(UserInput)){
+         listaNumeri.push(random[i]);   
+        }
     }
-}
+
+    const risultato = document.getElementById(`ultimoMex`);
+    risultato.innerHTML = `Hai indovinato ${listaNumeri.length} numeri: ${listaNumeri}`;
+
+}, 1000 * 31)
 
 
 
